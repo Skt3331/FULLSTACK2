14da4c1d41cb37e5be8233fa75cb9b837421a44e
SKT3331 <sauravkailastemgire@mitacsc.edu.in>
2024-03-01 19:22:02 +0530

    locals

--- Router-cookies-session-flash/connectflash.js
+++ Router-cookies-session-flash/connectflash.js
@@ -22,23 +22,57 @@ app.use(
 );
 app.use(flash());   
 
+app.use((req,res,next)=>
+{
+    res.locals.sucessmsf=req.flash("sucess");
+    res.locals.unsucessmsf=req.flash("error");
+    next();
+});
+
 app.get("/register",(req,res)=>
 {
-    // http://localhost:3000/register?name=saurav
-    let {name="anonymous"}=req.query;
-    req.session.name=name;
-    req.flash("sucess","user registed sucess");
-    res.redirect("/hello");
-});
-//with session id the session data will transfer variable
-app.get("/hello",(req,res)=>
-{
-//    res.send(`hello ${req.session.name}`); 
-//    console.log(req.flash("sucess"));
-   res.render("hellow.ejs",{name:req.session.name,msg:req.flash("sucess")});
+    let {name="anonyymus"}=req.query;
+    req.session.name=name; 
+    if (req.session.name==="anonyymus") {
+       msg=req.flash("error","user not be register");
+    }
+    else{
+       msg= req.flash("sucess","usser registred sucess");
+    }
+    res.redirect("/hellows");
 
-//    hello saurav
+    
 });
+app.get("/hellows",(req,res)=>
+{
+   
+   
+    res.render("hellow.ejs",{name:req.session.name});
+});
+
+
+// flash without local
+
+// app.get("/register",(req,res)=>
+// {
+//     // http://localhost:3000/register?name=saurav
+//     let {name="anonymous"}=req.query;
+//     req.session.name=name;
+//     req.flash("sucess","user registed sucess");
+//     res.redirect("/hello");
+// });
+// //with session id the session data will transfer variable
+// app.get("/hello",(req,res)=>
+// {
+// //    res.send(`hello ${req.session.name}`); 
+// //    console.log(req.flash("sucess"));
+//    res.render("hellow.ejs",{name:req.session.name,msg:req.flash("sucess")});
+
+// //    http://localhost:3000/register?name=saurav
+// // output
+// // user registed sucess   after refresh this flash will be disappere
+// // welcome saurav 
+// });
 
 
 
